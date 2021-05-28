b5b5a09249db0077e2bad4ff21279163d8881463
smirok <smirok123@mail.ru>
2021-05-28 13:56:05 +0300

    Added gtest

---
+++ test/testing.h
@@ -0,0 +1,9 @@
+#ifndef KEYVALUESTORAGE_TESTING_H
+#define KEYVALUESTORAGE_TESTING_H
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using namespace testing;
+
+#endif //KEYVALUESTORAGE_TESTING_H
