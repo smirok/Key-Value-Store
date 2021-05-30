275cc63f6ab5b190e13a1bb0079df5e7b0e6e72e
smirok <smirok123@mail.ru>
2021-05-30 23:07:05 +0300

    Added FileOffset and File with tests

---
+++ include/FileOffset.hpp
@@ -0,0 +1,20 @@
+#ifndef KEYVALUESTORAGE_FILEOFFSET_HPP
+#define KEYVALUESTORAGE_FILEOFFSET_HPP
+
+#include <cstddef>
+
+namespace kvs {
+
+    class FileOffset {
+    public:
+        FileOffset(std::size_t offset);
+
+        std::size_t getOffset() const;
+
+    private:
+        std::size_t _offset;
+    };
+
+}
+
+#endif //KEYVALUESTORAGE_FILEOFFSET_HPP
---
+++ src/FileOffset.cpp
@@ -0,0 +1,11 @@
+#include "FileOffset.hpp"
+
+namespace kvs {
+
+    FileOffset::FileOffset(std::size_t offset) : _offset(offset) {
+    }
+
+    std::size_t FileOffset::getOffset() const {
+        return _offset;
+    }
+}
