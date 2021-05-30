57e151cf61c179f7ee6b6786cc4e822d61f5262f
smirok <smirok123@mail.ru>
2021-05-30 17:15:07 +0300

    Added interface methods to KeyValueStore

--- include/KeyValueStore.hpp
+++ include/KeyValueStore.hpp
@@ -3,13 +3,27 @@
 
 #include <optional>
 #include "KeyValue.hpp"
+#include "BloomFilter.hpp"
 
 namespace kvs {
     class KeyValueStore {
     public:
+        KeyValueStore(const BloomFilter& bloomFilter);
+
         void add(const KeyValue &);
+
         std::optional<KeyValue> get(const Key &) const;
-        void del(const Key&);
+
+        void del(const Key &);
+
+        void clear();
+
+    private:
+        bool shouldRemoveOutdatedParts() const;
+
+        void removeOutdatedParts();
+
+        BloomFilter _bloomFilter;
     };
 }
 
