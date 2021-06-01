914bfb409f30a52c9f0d046b4e8cfe9aed4d564c
smirok <smirok123@mail.ru>
2021-06-01 13:19:44 +0300

    Added tests for Storage<Record>

--- include/Storage.hpp
+++ include/Storage.hpp
@@ -3,9 +3,7 @@
 
 #include "Id.hpp"
 #include "KeyValue.hpp"
-#include "TrieNode.hpp"
 #include "File.hpp"
-#include "TrieNodeSerializer.hpp"
 #include "RecordSerializer.hpp"
 #include <optional>
 
@@ -14,69 +12,64 @@ namespace kvs {
     template<typename T>
     class Storage {
     public:
-        virtual Id add(const T &) = 0;
+        Id add(const T &) {
+            return Id(0);
+        }
 
-        virtual std::optional<T> get(const Id &) = 0;
+        std::optional<T> get(const Id &) {
+            return std::nullopt;
+        };
 
-        virtual void remove(const Id &) = 0;
+        void remove(const Id &) {
+        }
 
-        virtual void clear() = 0;
+        void clear() {
+
+        }
     };
 
     template<>
     class Storage<Record> {
     public:
-        Storage(File &dataFile, const RecordSerializer &recordSerializer);
+        Storage(File &dataFile, const RecordSerializer &recordSerializer) : _dataFile(dataFile),
+                                                                            _recordSerializer(recordSerializer) {
+        }
 
-        Id add(const Record &record);
+        Id add(const Record &record) {
+            char *recordInBytes = _recordSerializer.recordToBytes(record);
+            FileOffset recordOffset = _dataFile.write(recordInBytes,
+                                                      _recordSerializer.getKeySize() + 1 +
+                                                      _recordSerializer.getValueSize()); // TODO
 
-        std::optional<Record> get(const Id &id);
+            return Id(recordOffset.getOffset() /
+                      (_recordSerializer.getKeySize() + 1 + _recordSerializer.getValueSize())); // TODO how should do it
+        }
 
-        void remove(const Id &id);
+        std::optional<Record> get(const Id &id) {
+            char *recordInBytes = _dataFile.read(_recordSerializer.getKeySize() + 1 + _recordSerializer.getValueSize(),
+                                                 FileOffset((_recordSerializer.getKeySize() + 1 +
+                                                             _recordSerializer.getValueSize()) *
+                                                            id.getId()));
 
-        void clear();
+            Record record = _recordSerializer.bytesToRecord(recordInBytes);
+            return record.getIsOutdated() ? std::nullopt : std::make_optional(record);
+        }
+
+        void remove(const Id &id) {
+            FileOffset bitOutdatedOffset =
+                    FileOffset(id.getId() * (_recordSerializer.getKeySize() + 1 + _recordSerializer.getValueSize()) +
+                               _recordSerializer.getKeySize());
+            _dataFile.writeByOffset(bitOutdatedOffset, new char[1]{'1'}, 1);
+        }
+
+        void clear() {
+            _dataFile.clear();
+        }
 
     private:
         File &_dataFile;
         RecordSerializer _recordSerializer;
     };
-
-    Storage<Record>::Storage(File &dataFile, const RecordSerializer &recordSerializer)
-            : _dataFile(dataFile),
-              _recordSerializer(recordSerializer) {
-    }
-
-    Id Storage<Record>::add(const Record &record) {
-        char *recordInBytes = _recordSerializer.recordToBytes(record);
-        FileOffset recordOffset = _dataFile.write(recordInBytes,
-                                                  _recordSerializer.getKeySize() + 1 +
-                                                  _recordSerializer.getValueSize()); // TODO
-
-        return Id(recordOffset.getOffset() /
-                  (_recordSerializer.getKeySize() + 1 + _recordSerializer.getValueSize())); // TODO how should do it
-    }
-
-    std::optional<Record> Storage<Record>::get(const Id &id) {
-        char *recordInBytes = _dataFile.read(_recordSerializer.getKeySize() + 1 + _recordSerializer.getValueSize(),
-                                             FileOffset((_recordSerializer.getKeySize() + 1 +
-                                                         _recordSerializer.getValueSize()) *
-                                                        id.getId()));
-
-        Record record = _recordSerializer.bytesToRecord(recordInBytes);
-        return record.getIsOutdated() ? std::nullopt : std::make_optional(record);
-    }
-
-    void Storage<Record>::remove(const Id &id) {
-        FileOffset bitOutdatedOffset =
-                FileOffset(id.getId() * (_recordSerializer.getKeySize() + 1 + _recordSerializer.getValueSize()) +
-                           _recordSerializer.getKeySize());
-        _dataFile.writeByOffset(bitOutdatedOffset, new char[1]{'1'}, 1);
-    }
-
-    void Storage<Record>::clear() {
-        _dataFile.clear();
-    }
 }
-
 
 #endif //KEYVALUESTORAGE_STORAGE_HPP
---
+++ test/RecordStorageTest.cpp
@@ -0,0 +1,76 @@
+#include "testing.h"
+
+#include "Storage.hpp"
+
+namespace kvs {
+
+    TEST(RecordStorage, creation) {
+        File file = File("data.bin");
+        RecordSerializer recordSerializer = RecordSerializer(4, 8);
+        Storage<Record>(file, recordSerializer);
+        remove("data.bin");
+    }
+
+    TEST(RecordStorage, addAndGet) {
+        File file = File("data.bin");
+        RecordSerializer recordSerializer = RecordSerializer(4, 8);
+        Storage<Record> storage(file, recordSerializer);
+
+        Record record(Key("aaaa", 4), false, Value("bbbbbbbb", 8));
+        Id id = storage.add(record);
+        EXPECT_EQ(id.getId(), 0);
+
+        std::optional<Record> takenRecordOptional = storage.get(id);
+        EXPECT_EQ(strcmp(record.getKey().getKey(), takenRecordOptional.value().getKey().getKey()), 0);
+        EXPECT_EQ(record.getIsOutdated(), takenRecordOptional.value().getIsOutdated());
+        EXPECT_EQ(strcmp(record.getValue().getValue(), takenRecordOptional.value().getValue().getValue()), 0);
+        remove("data.bin");
+    }
+
+    TEST(RecordStorage, autoIncrementAdd) {
+        File file = File("data.bin");
+        RecordSerializer recordSerializer = RecordSerializer(4, 8);
+        Storage<Record> storage(file, recordSerializer);
+
+        Record record(Key("aaaa", 4), false, Value("bbbbbbbb", 8));
+
+        for (int i = 0; i < 100; ++i) {
+            Id id = storage.add(record);
+            EXPECT_EQ(id.getId(), i);
+        }
+
+        remove("data.bin");
+    }
+
+    TEST(RecordStorage, getIfNotExist) {
+        File file = File("data.bin");
+        RecordSerializer recordSerializer = RecordSerializer(4, 8);
+        Storage<Record> storage(file, recordSerializer);
+
+        Record record(Key("aaaa", 4), false, Value("bbbbbbbb", 8));
+
+        Id id = storage.add(record);
+        EXPECT_EQ(id.getId(), 0);
+        std::optional<Record> takenRecordOptional = storage.get(Id(1));
+        EXPECT_FALSE(takenRecordOptional.has_value());
+
+        remove("data.bin");
+    }
+
+    TEST(RecordStorage, getIfWasRemoved) {
+        File file = File("data.bin");
+        RecordSerializer recordSerializer = RecordSerializer(4, 8);
+        Storage<Record> storage(file, recordSerializer);
+
+        Record record(Key("aaaa", 4), false, Value("bbbbbbbb", 8));
+
+        Id id = storage.add(record);
+        EXPECT_EQ(id.getId(), 0);
+        storage.remove(id);
+        std::optional<Record> takenRecordOptional = storage.get(Id(0));
+        EXPECT_FALSE(takenRecordOptional.has_value());
+
+        remove("data.bin");
+    }
+}
+
